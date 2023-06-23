882aa4d1b0a340e2d4da2d658dc401463a794be3
zanelladev <arturf.zanella@gmail.com>
2023-06-23 01:06:40 -0300

    style: new thme extension

--- lib/src/constants/theme/text/app_text.dart
+++ lib/src/constants/theme/text/app_text.dart
@@ -3,13 +3,19 @@ import 'package:flutter/material.dart';
 class AppText extends ThemeExtension<AppText> {
   const AppText({
     required this.buttonText,
+    required this.nunitoDefault,
   });
 
   final TextStyle buttonText;
+  final TextStyle nunitoDefault;
 
   @override
-  ThemeExtension<AppText> copyWith({TextStyle? buttonText}) {
-    return AppText(buttonText: buttonText ?? this.buttonText);
+  ThemeExtension<AppText> copyWith(
+      {TextStyle? buttonText, TextStyle? nunitoDefault}) {
+    return AppText(
+      buttonText: buttonText ?? this.buttonText,
+      nunitoDefault: nunitoDefault ?? this.nunitoDefault,
+    );
   }
 
   @override
@@ -19,16 +25,7 @@ class AppText extends ThemeExtension<AppText> {
     }
     return AppText(
       buttonText: buttonText,
-    );
-  }
-
-  factory AppText.mobile() {
-    return const AppText(
-      buttonText: TextStyle(
-        fontFamily: 'NunitoAsset',
-        fontSize: 13,
-        letterSpacing: 2,
-      ),
+      nunitoDefault: nunitoDefault,
     );
   }
 }
--- lib/src/constants/theme/theme_module.dart
+++
@@ -1,4 +0,0 @@
-library theme_module;
-
-export './colors/app_colors.dart';
-export './text/app_text.dart';
